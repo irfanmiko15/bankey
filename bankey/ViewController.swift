7cf6951fd55f827cc5337db25992c68db978df65
Irfan Dary <irfandarys@gmail.com>
2024-03-05 13:36:06 +0700

    Initial Commit

---
+++ bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bankey
+//
+//  Created by Irfan Dary Sujatmiko on 05/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
